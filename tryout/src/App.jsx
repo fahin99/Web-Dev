4bbf942edad8bfd706202add302d808e3c7ffd1a
fahin99 <abirlucario02@gmail.com>
2026-05-22 05:17:00 +0600

    feat: add Student component and integrate it into App; enhance CSS styling for student display

--- tryout/src/App.jsx
+++ tryout/src/App.jsx
@@ -4,13 +4,16 @@ import Food from "./Food.jsx";
 import Card from "./Card.jsx";
 import Button from "./Button/Button.jsx";
 import Button1 from "./Button1.jsx";
+import Student from "./Student.jsx";
 function App() {
   return(
     <>
       <Header/>
-      <Food/>
+      {/* <Food/>
       <Card/>
-      <Button/>
+      <Button/> */}
+      <Student name="Brown" age={10} isStudent={true}/>
+      <Student name="Bonito" age={20} isStudent={false}/>
       <p/>
       <Button1/>
       <Footer/>
